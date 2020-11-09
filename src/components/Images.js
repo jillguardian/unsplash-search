9fe98572c4f194c2f5e91a594bb08397ef73fa30
Angela Guardian <jillguardian@gmail.com>
2020-11-09 16:37:12 +0800

    Follow tutorial on grids

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Images.css';
 
 const Images = (props) => {
 
@@ -16,11 +17,44 @@ const Images = (props) => {
 
 export default Images;
 
-const Image = (props) => {
-    return (
-        <a href={props.urls.raw} className="ui medium image" target="_blank" rel="noreferrer">
-            <img className="ui medium rounded image" src={props.urls.thumb} alt={props.description} />
-        </a>
-    );
+class Image extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+        this.state = {spans: 0};
+    }
+
+    render() {
+        const {description, urls} = this.props;
+        return (
+            <a href={urls.raw}
+               className="ui medium image"
+               target="_blank"
+               rel="noreferrer"
+               style={{
+                   gridRowEnd: `span ${this.state.spans}`
+               }}>
+                <img ref={this.ref}
+                     className="ui medium rounded image"
+                     src={urls.thumb}
+                     alt={description} />
+            </a>
+        );
+    }
+
+    componentDidMount() {
+        const current = this.ref.current;
+        current.addEventListener('load', this.setGridSpan);
+    }
+
+    setGridSpan = () => {
+        const current = this.ref.current;
+        const height = current.clientHeight;
+        console.log(`${this.props.description}: ${height}`);
+
+        const spans = Math.ceil(height / 10);
+        this.setState({spans: spans});
+    }
 
 }
